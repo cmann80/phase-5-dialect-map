04f5d914cefb04b382fe88c838bc25a2dfe694e0
Chris Mann <chrispmann@gmail.com>
2022-12-05 09:37:35 -0800

    survey response page shows survey questions

---
+++ app/serializers/response_serializer.rb
@@ -0,0 +1,3 @@
+class ResponseSerializer < ActiveModel::Serializer
+  attributes :id, :r1, :r2, :r3, :r4, :r5, :r6, :r7, :r8, :r9, :r10, :user
+end
--- client/src/components/SurveyResponses/SurveyResponses.js
+++ client/src/components/SurveyResponses/SurveyResponses.js
@@ -1,17 +1,43 @@
 import { Link } from 'react-router-dom';
 import {useEffect, useState} from "react"
 
-function SurveyResponses({userLocations, survey}){
+function SurveyResponses({userLocations, survey, errors, setErrors}){
 
+const [responses, setResponses] = useState([])
+console.log(responses)
 
+useEffect(() => {
+    fetch(`/responses`)
+    .then(res => {
+        if(res.ok){
+            res.json().then(responseData => {
+                console.log(responseData)
+            setResponses(responseData)
+            })
+        } 
+        else{
+            res.json().then(data => setErrors(data.error))
+        }
+    })
+}, [])
 
 
 
     return (
 
 <div>
-    <h2>Survey Results</h2>
+    <h2>Survey Responses</h2>
     <br/>
+    <span>{survey.q1}</span><br/>
+    <span>{survey.q2}</span><br/>
+    <span>{survey.q3}</span><br/>
+    <span>{survey.q4}</span><br/>
+    <span>{survey.q5}</span><br/>
+    <span>{survey.q6}</span><br/>
+    <span>{survey.q7}</span><br/>
+    <span>{survey.q8}</span><br/>
+    <span>{survey.q9}</span><br/>
+    <span>{survey.q10}</span><br/>
     <Link to="/">back to home</Link>
 </div>
     )
